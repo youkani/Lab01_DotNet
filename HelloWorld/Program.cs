3c667081bd34a24500d9239d491f54b5863c6985
youkani <baaalena08@yandex.ru>
2026-09-12 10:56:48 +0300

    Step 6: first C# program running

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет от ИСП-242!");
+Console.WriteLine("Фвтор: Буничева Алёна");
+Console.WriteLine("Год: 2026");
